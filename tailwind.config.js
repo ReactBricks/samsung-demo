55af9e839c95d228bd80dd3239e8a24945c701bf
FrancescoMottaDev <francesco.motta@reactbricks.com>
2023-06-23 09:29:47 +0200

    define blueSamsung

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Nunito Sans', ...defaultTheme.fontFamily.sans],
-        samsung: ['Raleway']
+        samsung: ['Raleway'],
       },
       screens: {
         sm: '600px',
@@ -21,6 +21,27 @@ module.exports = {
       boxShadow: {
         newsLetter:
           '0 1px 3px 0 rgba(0, 0, 0, 0.1) , 0 5px 15px 0 rgba(0, 0, 0, 0.05)',
+      },
+      colors: {
+        blueSamsung: {
+          100: '#dbebff',
+
+          200: '#bed9ff',
+
+          300: '#96beff',
+
+          400: '#6d98ff',
+
+          500: '#4b72ff',
+
+          600: '#2b48ff',
+
+          700: '#2138e3',
+
+          800: '#1c30b7',
+
+          900: '#20328f',
+        },
       },
     },
   },
